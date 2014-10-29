8b6d7fde3dbd7086b57a04c604f896e6c9d01b13
Dvid Silva <spam@dvidsilva.com>
2014-10-29 15:25:22 -0700

    created the form, creates task in the db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,6 +3,10 @@ class TasksController < ApplicationController
   end
 
   def create
+    @task = Task.new(task_params)
+    respond_to do |format|
+      format.json { render json: @task }
+    end
   end
 
   def update
@@ -16,4 +20,10 @@ class TasksController < ApplicationController
 
   def destroy
   end
+
+  private
+    def task_params
+      params.require(:task).permit(:title, :description, :archived, :priority)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,7 @@
 Rails.application.routes.draw do
-  get 'tasks/create'
+  
+  resources :tasks, only: [:index, :new, :create, :update, :destroy]
 
-  get 'tasks/update'
-
-  get 'tasks/index'
-
-  get 'tasks/show'
-
-  get 'tasks/destroy'
-
-  get 'home/index'
   root 'home#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
